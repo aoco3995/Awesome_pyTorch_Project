01b7cdcd3ffddd6179a4c81bfcc51c8086176158
Joseph-Stolfa <joey.stolfa@okstate.edu>
2022-11-16 13:36:45 -0600

    Joeys Model

---
+++ Prototype_joeys/DatasetLoader.py
@@ -0,0 +1,60 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from Trainer import Trainer
+from TrainingAccuracy import TrainingAccuracy
+
+class ImageConfig():
+    def __init__(self):
+
+        transform = transforms.Compose(
+            [transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                                download=True, transform=transform)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                                shuffle=True, num_workers=2)
+
+        testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                            download=True, transform=transform)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                                shuffle=False, num_workers=2)
+
+        classes = ('plane', 'car', 'bird', 'cat',
+                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+
+        # functions to show an image
+
+
+        def imshow(img):
+            img = img / 2 + 0.5     # unnormalize
+            npimg = img.numpy()
+            plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+
+        # get some random training images
+        dataiter = iter(trainloader)
+        images, labels = next(dataiter)
+
+        # show images
+        imshow(torchvision.utils.make_grid(images))
+        # print labels
+        print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+        plt.show()
+
+        trainer = Trainer(trainloader)
+
+        trainingAccuracy = TrainingAccuracy()
+        trainingAccuracy.accuracy(trainer,testloader)
+        trainingAccuracy.outputs()
+
+
+if __name__ == '__main__':
+    imageConfig = ImageConfig()
---
+++ Prototype_joeys/TrainingAccuracy.py
@@ -0,0 +1,21 @@
+import torch
+
+class TrainingAccuracy():
+    def __init__(self):
+
+        self.correct = 0
+        self.total = 0
+
+    def accuracy(self,trainer,testloader):
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data
+                outputs = trainer.outputs(images)
+                _, predicted = torch.max(outputs.data, 1)
+                self.total += labels.size(0)
+                self.correct += (predicted == labels).sum().item()
+
+    def outputs(self):
+
+        print('Accuracy of the network on the 10000 test images: %d %%' % (
+               100 * self.correct / self.total))
