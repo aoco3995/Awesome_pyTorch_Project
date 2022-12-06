54818d93f53c8f33b5fd53cb85351d30eec812c8
Adam O'Connor <adamoconnor32@gmail.com>
2022-12-05 20:57:29 -0600

    scaled logic

--- bound_box.py
+++ bound_box.py
@@ -1,6 +1,11 @@
 import cv2
 import numpy as np
 import copy
+import os
+ 
+dir = 'crops'
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))
 
 # read and scale down image
 # wget https://bigsnarf.files.wordpress.com/2017/05/hammer.png #black and white
@@ -13,16 +18,16 @@ threshed_img =[]
 for i in range(5):
     images.append(copy.deepcopy(img))
 
-i = 1
+i = 0
 for image in images:
     # threshold image
-    threshed_img.append(cv2.threshold(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), i*50, 255, cv2.THRESH_BINARY)[1])
+    threshed_img.append(cv2.threshold(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), i*20+77, 255, cv2.THRESH_BINARY)[1])
     i = i + 1
 
 i = 1
-for threshed in threshed_img:
-    cv2.imshow("threshhold Image" + str(i), threshed)
-    i = i + 1
+# for threshed in threshed_img:
+#     cv2.imshow("threshhold Image" + str(i), threshed)
+#     i = i + 1
 
 # find contours and get the external one
 
@@ -35,24 +40,24 @@ for threshed in threshed_img:
     # with each contour, draw boundingRect in green
     # a minAreaRect in red and
     # a minEnclosingCircle in blue
-    print(contours)
+    #print(contours)
     for c in contours:
         # get the bounding rect
         x, y, w, h = cv2.boundingRect(c)
-        if h >= 50:
+        if (h > img.shape[0]/8) & (w > img.shape[1]/8):
             crop_img = orig_img[y:y+h, x:x+w]
             cv2.imwrite("crops\\"+str(x)+str(y)+".png", crop_img)
-            print(h,w)
+            #print(h,w)
             # draw a green rectangle to visualize the bounding rect
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    print(len(contours))
+    #print(len(contours))
     cv2.drawContours(img, contours, -1, (255, 255, 0), 1)
 
     cv2.imshow("contours", img)
 
     cv2.imshow("contours", img)
-    print(type(contours))
+    #print(type(contours))
 
 while True:
     key = cv2.waitKey(1)
